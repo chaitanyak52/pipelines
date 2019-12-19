cae5d12fc216e31151b75118af8144263dcb0356
Alejandro Campos Magencio (ALEX) <alejacma@microsoft.com>
2019-12-19 17:20:37 +0100

    Adds Workspace Item Ops to Azure Databricks for KFP

--- samples/contrib/azure-samples/databricks-pipelines/databricks_workspaceitem_pipeline.py
+++ samples/contrib/azure-samples/databricks-pipelines/databricks_workspaceitem_pipeline.py
@@ -1,52 +1,22 @@
 """Import an item into a Databricks Workspace."""
 import kfp.dsl as dsl
 import kfp.compiler as compiler
+import databricks
 
-# https://docs.databricks.com/dev-tools/api/latest/workspace.html#import
-# path	STRING	The absolute path of the notebook or directory. Importing directory is only support for DBC format. This field is required.
-# format	ExportFormat	This specifies the format of the file to be imported. By default, this is SOURCE. However it may be one of: SOURCE, HTML, JUPYTER, DBC. The value is case sensitive.
-# language	Language	The language. If format is set to SOURCE, this field is required; otherwise, it will be ignored.
-# content	BYTES	The base64-encoded content. This has a limit of 10 MB. If the limit (10MB) is exceeded, exception with error code MAX_NOTEBOOK_SIZE_EXCEEDED will be thrown. This parameter might be absent, and instead a posted file will be used. See Import a notebook or directory for more information about how to use it.
-# overwrite	BOOL	The flag that specifies whether to overwrite existing object. It is false by default. For DBC format, overwrite is not supported since it may contain a directory.
 def import_workspace_item(item_name):
-    return dsl.ResourceOp(
+    return databricks.ImportWorkspaceItemOp(
         name="importworkspaceitem",
-        k8s_resource={
-            "apiVersion": "databricks.microsoft.com/v1alpha1",
-            "kind": "WorkspaceItem",
-            "metadata": {
-                "name": item_name
-            },
-            "spec":{
-                "content": "MSsx",
-                "path": "/Users/alejacma@microsoft.com/ScalaExampleNotebook",
-                "language": "SCALA",
-                "overwrite": True,
-                "format": "SOURCE"
-            }
-        },
-        action="create",
-        success_condition="status.object_hash",
-        attribute_outputs={
-            "name": "{.metadata.name}",
-            "object_hash": "{.status.object_hash}",
-            "object_language": "{.status.object_info.language}",
-            "object_type": "{.status.object_info.object_type}",
-            "object_path": "{.status.object_info.path}"
-        }
+        item_name=item_name,
+        content="cHJpbnQoImhlbGxvLCB3b3JsZCIpCgoK",
+        path="/Users/alejacma@microsoft.com/ScalaExampleNotebook",
+        language="SCALA",
+        file_format="SOURCE"
     )
 
 def delete_workspace_item(item_name):
-    return dsl.ResourceOp(
+    return databricks.DeleteWorkspaceItemOp(
         name="deleteworkspaceitem",
-        k8s_resource={
-            "apiVersion": "databricks.microsoft.com/v1alpha1",
-            "kind": "WorkspaceItem",
-            "metadata": {
-                "name": item_name,
-            }
-        },
-        action="delete"
+        item_name=item_name
     )
 
 @dsl.pipeline(
--- samples/contrib/azure-samples/kfp-azure-databricks/databricks/__init__.py
+++ samples/contrib/azure-samples/kfp-azure-databricks/databricks/__init__.py
@@ -2,5 +2,6 @@ from ._job_op import CreateJobOp, DeleteJobOp
 from ._run_op import SubmitRunOp, DeleteRunOp
 from ._cluster_op import CreateClusterOp, DeleteClusterOp
 from ._secretscope_op import CreateSecretScopeOp, DeleteSecretScopeOp
+from ._workspaceitem_op import ImportWorkspaceItemOp, DeleteWorkspaceItemOp
 
 __version__ = "0.2.0"
